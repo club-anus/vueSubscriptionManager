c0856553630c1352f1bb04e7ddddda45b37a2ab1
youtangai <youtanagai@gmail.com>
2018-12-02 20:32:19 +0900

    表示カードの実装

---
+++ src/components/SubscriptionCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div>{{title}}</div>
+        <div>{{amount}}円</div>
+        <button @click="goToUpdate(id, title, amount)">編集</button>
+        <button @click="del(id)">削除</button>
+    </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+
+export default {
+    props: {
+        id: Number,
+        title: String,
+        amount: Number
+    },
+    methods: {
+        ...mapActions({
+            del: 'subscriptionDel'
+        }),
+        goToUpdate: (id, title, amount) => {
+            const view = { name: 'edit', params: { id, title, amount }}
+            this.$route.push(view)
+        }
+    }
+}
+</script>
