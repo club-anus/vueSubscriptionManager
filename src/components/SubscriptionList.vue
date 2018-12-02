afb158c749c2e0ba481ccd7baff4e9ef1a6eda79
youtangai <youtanagai@gmail.com>
2018-12-02 22:04:09 +0900

    構成を変更

---
+++ src/components/SubscriptionList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <card 
+            v-for="item in list" 
+            v-bind:key="item.id"
+            :id="item.id"
+            :title="item.title"
+            :amount="item.amount"
+        />
+    </div>
+</template>
+
+<script>
+import Card from './SubscriptionCard'
+import {mapGetters} from 'vuex'
+
+export default {
+    components: {
+        'card': Card
+    },
+    computed: {
+        ...mapGetters({
+            list: 'subscriptionList'
+        })
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <list />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import List from '@/components/SubscriptionList.vue'
 
 export default {
-  name: 'home',
   components: {
-    HelloWorld
+    'list': List
   }
 }
 </script>
