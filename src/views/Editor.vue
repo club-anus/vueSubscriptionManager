2bd69774f662ef5853a0621baa09c5cdaacf68c7
youtangai <youtanagai@gmail.com>
2018-12-02 18:23:42 +0900

    fix bug

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <input type="text" v-model="title"/><br>
-        <input type="number" v-model="amount" /><br>
-        <button v-if="Id == null" @click="add({ Title, Amount })">追加</button>
+        <input type="text" v-model="Title"/><br>
+        <input type="number" v-model="Amount" /><br>
+        <button v-if="Id == null" @click="add({ title:Title, amount:Amount })">追加</button>
         <button v-else @click="upd({ Id, Title, Amount })">変更</button>
     </div>
 </template>
@@ -18,11 +18,10 @@ export default {
         amount: Number
     },
     data() {
-        console.log(this)
         return {
             Id: this.id,
             Title: this.title == null ? "": this.title,
-            Amount: this.amount == null ? 0: this.title
+            Amount: this.amount == null ? 0: this.amount
         }
     },
     methods: {
