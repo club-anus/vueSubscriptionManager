b9354f323ddb6686997814ac73863a8e99da26c4
youtangai <youtanagai@gmail.com>
2018-12-05 14:03:54 +0900

    dialogの追加

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { MdButton, MdCard, MdContent, MdIcon, MdField } from 'vue-material/dist/components'
+import { MdButton, MdCard, MdContent, MdIcon, MdField, MdDialog } from 'vue-material/dist/components'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css' // This line here
 
@@ -11,6 +11,7 @@ Vue.use(MdCard)
 Vue.use(MdContent)
 Vue.use(MdIcon)
 Vue.use(MdField)
+Vue.use(MdDialog)
 
 Vue.config.productionTip = false
 
